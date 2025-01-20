a817ee067f48a7de61e35c4930d6f02922747469
Nkatha <nkatha@Winnies-MacBook-Air.local>
2025-01-21 00:03:22 +0300

    get stock data and convert to csv

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -95,6 +95,21 @@ export interface Media {
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
+ * via the `definition` "StockRequest".
+ */
+export interface StockRequest {
+  id: string;
+  alt: string;
+  updatedAt: string;
+  createdAt: string;
+  companySymbol: string;
+  startDate: string;
+  endDate: string;
+  email: string;
+  status: 'pending' | 'completed' | 'failed';
+};
+/**
+ * This interface was referenced by `Config`'s JSON-Schema
  * via the `definition` "payload-locked-documents".
  */
 export interface PayloadLockedDocument {
