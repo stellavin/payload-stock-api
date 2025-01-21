abda9dbe8f917f8f97923306a425fbe22bad6790
Nkatha <nkatha@Winnies-MacBook-Air.local>
2025-01-21 17:03:39 +0300

    fixed data

--- src/email/transport.ts
+++ src/email/transport.ts
@@ -1,18 +1,18 @@
-import nodemailer from 'nodemailer';
+import nodemailer from 'nodemailer'
 
 /**
  * Creates and configures a Nodemailer transporter for sending emails via Gmail's SMTP server.
- * 
- * The transporter is configured to use OAuth2 authentication with credentials provided 
+ *
+ * The transporter is configured to use OAuth2 authentication with credentials provided
  * via environment variables. Ensure that the required environment variables are set:
  * - `SMTP_PORT`: The SMTP port (typically 465 for secure connections).
  * - `CLIENT_ID`: The OAuth2 client ID.
  * - `CLIENT_SECRET`: The OAuth2 client secret.
- * 
+ *
  * @returns A pre-configured Nodemailer transporter instance.
  */
-let transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
   port: process.env.SMTP_PORT, // SMTP port (e.g., 465)
   secure: true, // Use SSL for secure connections
   auth: {
@@ -20,6 +20,6 @@ let transporter = nodemailer.createTransport({
     clientId: process.env.CLIENT_ID, // OAuth2 client ID
     clientSecret: process.env.CLIENT_SECRET, // OAuth2 client secret
   },
-});
+})
 
-export default transporter;
+export default transporter
